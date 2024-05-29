31c9bf94265c9a79755d20184c62be22b531ba64
iamrenaud <renaudwoanapro@gmail.com>
2024-05-29 16:33:00 +0000

    simple structure for webhook templates

---
+++ src/types/Templates.ts
@@ -0,0 +1,25 @@
+const templates = {
+    "stripe": {
+        name: "stripe-xxxxx",
+        auth: {
+            type: "hmac",
+        },
+    },
+    "apiframe": {
+        name: "apiframe-xxxxx",
+        auth: {
+            type: "api_key",
+        },
+    }, 
+    "slack": {
+       
+    },
+    "gmail": {
+
+    },
+    "discord": {
+
+    }
+};
+
+export default templates;
